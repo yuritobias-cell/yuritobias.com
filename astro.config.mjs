16ab7dd808d65d60fe1d122665107677f5679b0b
Yuri Tobias <yuritobias@gmail.com>
2026-05-11 21:49:38 -0300

    add sitemap integration to astro.config.mjs and add revision materials for 1st stage evaluations in 1em, 2em and 9ano. Also update the descriptions of the first two evaluations in 9ano.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://yuritobias.com',
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
